2dbb1be7bde47a0a59224a6ff61c0c8b5a7ddc6f
diii2409 <duynhut366@gmail.com>
2024-06-25 15:43:53 +0700

    responsive header menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,23 @@
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
+	content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
 	theme: {
 		extend: {
+			fontFamily: {
+				Karla: ["Karla", "sans-serif"],
+			},
 			colors: {
 				"light-coffee": "#C89F94",
+			},
+		},
+		animation: {
+			slideDown: "slideDown .4s ease-in-out",
+		},
+		keyframes: {
+			slideDown: {
+				"0%": { transform: "translateY(-100%)" },
+				"100%": { transform: "translateY(0)" },
 			},
 		},
 	},
